82e2bcdf8f2a379f1908474ce8df6ddb33a44c57
Jumper <911079536@qq.com>
2022-10-30 21:29:01 +0800

    config

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,59 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+//! Renders a 2D scene containing a single, moving sprite.
+
+// disable console on windows for release builds
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use bevy::prelude::*;
+
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
 
 fn main() {
+    let height = 900.0;
     App::new()
+        .insert_resource(WindowDescriptor {
+            width: height * ASPECT_RATIO,
+            height,
+            position: WindowPosition::Automatic,
+            title: "bevy_game".to_string(),
+            resizable: false,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_system(sprite_movement)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
+#[derive(Component)]
+enum Direction {
+    Up,
+    Down,
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
-
-    // Rectangle
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(50.0, 50.0)),
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: asset_server.load("textures\\xiong.png"),
+            transform: Transform::from_xyz(0., 0., 0.),
             ..default()
-        },
-        ..default()
-    });
+        })
+        .insert(Direction::Down);
+}
 
-    // Circle
-    commands.spawn_bundle(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-100., 0., 0.)),
-        ..default()
-    });
+/// The sprite is animated by changing its translation depending on the time that has passed since
+/// the last frame.
+fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut logo, mut transform) in &mut sprite_position {
+        match *logo {
+            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
+            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
+        }
 
-    // Hexagon
-    commands.spawn_bundle(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::RegularPolygon::new(50., 6).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
-        transform: Transform::from_translation(Vec3::new(100., 0., 0.)),
-        ..default()
-    });
+        if transform.translation.y > 200. {
+            *logo = Direction::Down;
+        } else if transform.translation.y < -200. {
+            *logo = Direction::Up;
+        }
+    }
 }
